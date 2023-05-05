7daad0661a498ff34bd299384cfd5717a5e6742c
ananasovitch <118258154+ananasovitch@users.noreply.github.com>
2023-05-05 17:16:39 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -15,7 +15,7 @@ public class Main {
         if (!operator.matches("[+\\-*/]")) throw new Exception("Неподдерживаемая математическая операция");
         if (operands.length != 3) throw new Exception("Должно быть два операнда");
         if (operator == null) throw new Exception("Неподдерживаемая математическая операция");
-        if (a<0 || b<0 || a>10 || b>10 ) {throw new Exception("Числа от 1 до 10");
+        if (a<=0 || b<0 || a>10 || b>10 ) {throw new Exception("Числа от 1 до 10");
         }
 
         switch (operator) {
